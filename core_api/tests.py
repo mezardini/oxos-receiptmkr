4dc963b2ffd055bdc4712045206912f5db29bccc
mezeiser <mezardini@gmail.com>
2023-06-15 19:37:06 +0100

    wrote unit tests

--- core_api/tests.py
+++ core_api/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from .models import ReceiptRequest
+from django.contrib.auth.models import User
+
+
+class ReceiptRequestTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data for the tests
+        
+        ReceiptRequest.objects.create(receipt_name='mezard', user_no=9)
+
+    def test_your_model_method(self):
+        obj = ReceiptRequest.objects.get(receipt_name='mezard')
+        # Test the behavior of your model method
+        self.assertEqual(obj.__str__(), 'mezard by 9')
+
+class ReceiptRequestTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data for the tests
+        
+        ReceiptRequest.objects.create(receipt_name='mezard', user_no=9)
+
+    def test_your_model_method(self):
+        obj = ReceiptRequest.objects.get(receipt_name='mezard')
+        # Test the behavior of your model method
+        self.assertEqual(obj.__str__(), 'mezard by 9')
--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from .models import Seller, PaymentLogs
+from django.contrib.auth.models import User
+
+
+class SellerTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data for the tests
+        
+        user = User.objects.create_user(username='mezard', password='mezard2001')
+        userx = User.objects.get(username='mezard')
+        Seller.objects.create(biz_code='BC238929', user=userx, receipt_allocation=10)
+
+    def test_your_model_method(self):
+        obj = Seller.objects.get(biz_code='BC238929')
+
+        # Test the behavior of your model method
+        self.assertEqual(obj.__str__(), obj.biz_code)
+        print(obj.biz_code)
+
+class PaymentLogTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data for the tests
+        
+        user = User.objects.create_user(username='mezard', password='mezard2001')
+        userx = User.objects.get(username='mezard')
+        PaymentLogs.objects.create(transaction_id='BC238929', payer=userx, amount=10, status='SUCCESS')
+
+    def test_your_model_method(self):
+        obj = PaymentLogs.objects.get(transaction_id='BC238929')
+
+        # Test the behavior of your model method
+        self.assertEqual(obj.__str__(), f"{obj.payer.username} - {obj.amount}")
+
+        print(f"{obj.payer.username} - {obj.amount}")
