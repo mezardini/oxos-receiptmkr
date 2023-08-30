ccd6e9c27d95eaa61b1ae843e88bdd7dc2caa517
mezeiser <mezardini@gmail.com>
2023-08-30 06:31:22 +0100

    preparing for production

--- receiptmkr/production.py
+++ receiptmkr/production.py
@@ -6,9 +6,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['oxos-receiptmkr.onrender.com']
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
